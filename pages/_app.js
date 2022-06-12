5113a6d3cc2c0354ef1cc63f94949191ca0e9e46
ASchwartzman <aleschwartzman22@gmail.com>
2022-06-12 15:05:12 -0300

    Project working

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { MoralisProvider } from "react-moralis";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <MoralisProvider
+      appId='mEAiB0m5g6TxtsmrG17v1fHWldY1ItYDOmMKAe3p'
+      serverUrl='https://u5kkn03mvzrh.usemoralis.com:2053/server'
+    >
+      <Component {...pageProps} />;
+    </MoralisProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
